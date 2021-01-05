094a6e3b7061df8588e395868948919ddd5d3940
Matthew Jones <mejones92@gmail.com>
2021-01-05 21:08:08 +0000

    First commit of scripts folder containing JavaScript

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+
+// Class and constructor for quiz questions
+class quiz {
+    constructor(question, answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
+
+// Quiz questions and answers
+const questionOne = new quiz('Question 1: Which of these is a data structure?', 'Array');
+const questionTwo = new quiz('Question 2: Which of these is used to add conditions to JavaScript code?', 'If/Else');
+const questionThree = new quiz('Question 3: JavaScript uses these for repeating code', 'Functions');
+const questionFour = new quiz('Question 4: Key value pairs are commonly used with which data structure?', 'Objects');
+const questionFive = new quiz('Question 5: What do we end a JavaScript statement with?', 'Semi-colon');
+
+function results() {
+
+}
+
+function nextQuestion() {
+
+}
