1bb3cfe02e4301ab10866c9f4916015fa6153461
Matthew Jones <mejones92@gmail.com>
2021-01-09 21:50:55 +0000

    Additional code blocks added to JavaScript file to extend quiz app functionality.

--- scripts/app.js
+++ scripts/app.js
@@ -1,130 +1,137 @@
-// Global variables
+// Button variables
 var startBtn = document.getElementById('start-button');
 var nextBtn = document.getElementById('next-button');
-var prevBtn = document.getElementById('previous-button');
 
-// Event listeners
+// Button event listeners
 startBtn.addEventListener('click', countDown);
-startBtn.addEventListener('click', loadQuestion);
-nextBtn.addEventListener('click', loadQuestion);
-nextBtn.addEventListener('click', countDown);
-nextBtn.addEventListener('click', nextButtonCounter);
-prevBtn.addEventListener('click', prevButtonCounter);
+startBtn.addEventListener('click', randomQuestion);
+nextBtn.addEventListener('click', randomQuestion);
 
 // Countdown timer function
 function countDown() {
-  var timeLeftDisplay = document.getElementById('time');
-  var timeLeft = 10;
+  var timeCounter = document.getElementById('time');
+  var timeTotal = 30;
   setInterval(function() {
-    if (timeLeft <= 0) {
-        clearInterval(timeLeft = 0);
+    if (timeTotal < 0) {
+      clearInterval(timeTotal = 0);
     }
-    timeLeftDisplay.innerHTML = timeLeft;
-    timeLeft -= 1;
+    timeCounter.innerHTML = timeTotal;
+    timeTotal -= 1;
   }, 1000)
-}
-
-// Functions to log what question user is on
-var numOfNextClicks = 0;
-
-function nextButtonCounter() {
-  numOfNextClicks++;
-  var counter = document.getElementById('counter')
-  counter.innerHTML = numOfNextClicks;
-}
-
-function prevButtonCounter() {
-  numOfNextClicks--;
-  var counter = document.getElementById('counter')
-  counter.innerHTML = numOfNextClicks;
-}
-
-// Randon question generator
-function randonQuestion() {
-  var randNum = Math.floor(Math.random() * 4);
-  var questNum = questionObj[randNum].question;
-  var quizQuestion = document.getElementById('quiz-question');
-  quizQuestion.innerHTML = questNum;
 }
 
 // Questions and answers object
 var questionObj = [
   {
-    question: 'Question 1: What is an array?',
+    number: 1,
+    question: 'What is an array?',
     answers: ['a data structure', 'a function', 'a condition', 'a method']
   },
   {
-    question: 'Question 2: Which of these is used to add conditions to JavaScript code?',
+    number: 2,
+    question: 'Which of these is used to add conditions to JavaScript code?',
     answers: ['full-stop', 'if/else statement', 'array', 'template literal']
   },
   {
-    question: 'Question 3: What does JavaScript use for repeating code with a condition?',
+    number: 3,
+    question: 'What does JavaScript use for repeating code with a condition?',
     answers: ['key-value pairs', 'variables', 'loops', 'functions']
   },
   {
-    question: 'Question 4: A class constructor is used in conjunction with what data structure?',
+    number: 4,
+    question: 'A class constructor is used in conjunction with what data structure?',
     answers: ['AJAX', 'async await', 'objects', 'DOM']
   }
 ]
 
+// Random question generator
+function randomQuestion() {
+  var randNum = Math.floor(Math.random() * questionObj.length);
+  var randQuest = questionObj[randNum].question;
+  var quizQuestion = document.getElementById('quiz-question');
+  quizQuestion.innerHTML = randQuest;
+
+  var answer1 = document.getElementById('answ1');
+  var answer2 = document.getElementById('answ2');
+  var answer3 = document.getElementById('answ3');
+  var answer4 = document.getElementById('answ4');
+  var score = document.getElementById('outcome');
+
+  nextBtn.addEventListener('click', function() {score.innerHTML = ""})
+  
+  if (randNum === 0) {
+    answer1.innerHTML = questionObj[0].answers[0];
+    answer1.addEventListener('click', function() {
+      score.innerHTML = 'Correct!';
+    })
+    answer2.innerHTML = questionObj[0].answers[1];
+    answer2.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer3.innerHTML = questionObj[0].answers[2];
+    answer3.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer4.innerHTML = questionObj[0].answers[3];
+    answer4.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+  }
+  else if (randNum === 1) {
+    answer1.innerHTML = questionObj[1].answers[0];
+    answer1.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer2.innerHTML = questionObj[1].answers[1];
+    answer2.addEventListener('click', function() {
+      score.innerHTML = 'Correct!';
+    })
+    answer3.innerHTML = questionObj[1].answers[2];
+    answer3.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer4.innerHTML = questionObj[1].answers[3];
+    answer4.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+  }
+  else if (randNum === 2) {
+    answer1.innerHTML = questionObj[2].answers[0];
+    answer1.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer2.innerHTML = questionObj[2].answers[1];
+    answer2.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer3.innerHTML = questionObj[2].answers[2];
+    answer3.addEventListener('click', function() {
+      score.innerHTML = 'Correct!';
+    })
+    answer4.innerHTML = questionObj[2].answers[3];
+    answer4.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+  }
+  else if (randNum === 3) {
+    answer1.innerHTML = questionObj[3].answers[0];
+    answer1.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer2.innerHTML = questionObj[3].answers[1];
+    answer2.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+    answer3.innerHTML = questionObj[3].answers[2];
+    answer3.addEventListener('click', function() {
+      score.innerHTML = 'Correct!';
+    })
+    answer4.innerHTML = questionObj[3].answers[3];
+    answer4.addEventListener('click', function() {
+      score.innerHTML = 'False!';
+    })
+  }
+}
 
 
 
-
-// // Questions and answers object 1
-// var questionObj1 = [
-//   {
-//       question: 'Question 1: What is an array?',
-//       answers: [
-//           {text: 'a data structure', correct: true},
-//           {text: 'a function', correct: false},
-//           {text: 'a condition', correct: false},
-//           {text: 'a method', correct: false}
-//       ]
-//   }
-// ]
-
-// // Questions and answers object 2
-// var questionObj2 = [
-//   {
-//       question: 'Question 2: Which of these is used to add conditions to JavaScript code?',
-//       answers: [
-//           {text: 'full-stop', correct: false},
-//           {text: 'if/else statement', correct: true},
-//           {text: 'array', correct: false},
-//           {text: 'template literal', correct: false}
-//       ]
-//   }
-// ]
-
-// // Questions and answers object 3
-// var questionObj3 = [
-//   {
-//       question: 'Question 3: What does JavaScript use for repeating code with a condition?',
-//       answers: [
-//           {text: 'key-value pairs', correct: false},
-//           {text: 'variables', correct: false},
-//           {text: 'loops', correct: true},
-//           {text: 'functions', correct: false}
-//       ]
-//   }
-// ]
-
-// // Questions and answers object 4
-// var questionObj4 = [
-//   {
-//       question: 'Question 4: A class constructor is used in conjunction with what data structure?',
-//       answers: [
-//           {text: 'AJAX', correct: false},
-//           {text: 'async await', correct: false},
-//           {text: 'objects', correct: true},
-//           {text: 'DOM', correct: false}
-//       ]
-//   }
-// ]
-
-// // Function to load question one
-// function loadQuestion() {
-//   var quizQuestion = document.getElementById('quiz-question');
-//   quizQuestion.innerHTML = questionObj1[0].question;
-// }
